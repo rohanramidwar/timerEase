6ce66b240ef4d1a1cd92f3e2f730e566568d98f1
RohanRamidwar <rohanramidwar71@gmail.com>
2023-07-18 22:48:23 +0530

    add logic

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,8 +1,21 @@
-var secRemaining=180;
-var minutes=secRemaining/60;
-var seconds=secRemaining%60;
+// prompt("ENTER START VALUE: ")
+const startTime = 10;
+let time = startTime * 60;
 
-setInterval(timer(secRemaining),1000);
+const countdownEl = document.getElementById("countdown");
+setInterval(updateCountdown,1000);
 
-console.log(minutes);
-console.log(seconds);
+function restart()
+{
+    time = startTime * 60;
+}
+
+function updateCountdown()
+{
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = (seconds<10) ? "0" + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
